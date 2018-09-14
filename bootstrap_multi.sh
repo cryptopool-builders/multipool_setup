7e858849424192c68c1518384f7c8d001c02368a
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-14 18:31:30 -0400

    Create bootstrap_multi.sh

---
+++ bootstrap_multi.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#########################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
+# Updated by cryptopool.builders for crypto use...
+# This script is intended to be ran from the multipool installer
+#########################################################
+
+if [ -z "$TAG" ]; then
+	TAG=v1.0
+fi
+
+# Clone the MultiPool repository if it doesn't exist.
+if [ ! -d $HOME/multipool/yiimp_multi ]; then
+	echo Downloading MultiPool YiiMP Multi Server Installer $TAG. . .
+	git clone \
+		-b $TAG --depth 1 \
+		https://github.com/cryptopool-builders/multipool_yiimp_multi \
+		$HOME/multipool/yiimp_multi \
+		< /dev/null 2> /dev/null
+
+	echo
+fi
+
+# Change directory to it.
+cd $HOME/multipool/yiimp_multi
+
+# Update it.
+if [ "$TAG" != `git describe --tags` ]; then
+	echo Updating MultiPool YiiMP Multi Server Installer to $TAG . . .
+	git fetch --depth 1 --force --prune origin tag $TAG
+	if ! git checkout -q $TAG; then
+		echo "Update failed. Did you modify something in `pwd`?"
+		exit
+	fi
+	echo
+fi
+
+# Start setup script.
+cd $HOME/multipool/yiimp_multi
+source start.sh
