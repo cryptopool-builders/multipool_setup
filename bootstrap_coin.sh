855af5166161c90f268ff6b6ccfd45610705c6ca
cryptopool <cryptopool.builders@gmail.com>
2018-09-17 14:32:04 -0400

    update
    update

---
+++ bootstrap_coin.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#########################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
+# Updated by cryptopool.builders for crypto use...
+# This script is intended to be ran from the multipool installer
+#########################################################
+
+if [ -z "$TAG" ]; then
+	TAG=v1.0
+fi
+
+# Clone the MultiPool repository if it doesn't exist.
+if [ ! -d $HOME/multipool/daemon_builder ]; then
+	echo Downloading MultiPool Daemon Builder Installer $TAG. . .
+	git clone \
+		-b $TAG --depth 1 \
+		https://https://github.com/cryptopool-builders/multipool_coin_builder \
+		$HOME/multipool/daemon_builder \
+		< /dev/null 2> /dev/null
+
+	echo
+fi
+
+# Change directory to it.
+cd $HOME/multipool/daemon_builder
+
+# Update it.
+if [ "$TAG" != `git describe --tags` ]; then
+	echo Updating Daemon Builder Installer to $TAG . . .
+	git fetch --depth 1 --force --prune origin tag $TAG
+	if ! git checkout -q $TAG; then
+		echo "Update failed. Did you modify something in `pwd`?"
+		exit
+	fi
+	echo
+fi
+
+# Start setup script.
+cd $HOME/multipool/daemon_builder
+source install.sh
