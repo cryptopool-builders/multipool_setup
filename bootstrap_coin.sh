47f19087b2ab5b0312b6c5d171525d12535376b8
cryptopool <cryptopool.builders@gmail.com>
2018-11-28 19:54:19 -0500

    Update bootstrap_coin.sh

--- bootstrap_coin.sh
+++ bootstrap_coin.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-
-
 #########################################################
 # Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
 # Updated by cryptopool.builders for crypto use...
 # This script is intended to be ran from the multipool installer
 #########################################################
 
-if [ -z "{$TAG}" ]; then
-	TAG=v1.17
+if [ -z "${TAG}" ]; then
+	TAG=v1.18
 fi
 
 # Clone the MultiPool repository if it doesn't exist.
