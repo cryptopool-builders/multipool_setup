f224f83e911e50a8c03c0c67cf6941049edff271
cryptopool <cryptopool.builders@gmail.com>
2019-02-27 15:12:56 -0500

    fix balloon build error

--- bootstrap_multi.sh
+++ bootstrap_multi.sh
@@ -8,7 +8,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v1.26
+	TAG=v1.27
 fi
 
 # Clone the MultiPool repository if it doesn't exist.
--- bootstrap_single.sh
+++ bootstrap_single.sh
@@ -8,7 +8,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v1.20
+	TAG=v1.21
 fi
 
 # Clone the MultiPool repository if it doesn't exist.
