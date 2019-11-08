a66e5c95921832ea69ad476a03f13e9c758aa782
cryptopool <cryptopool.builders@gmail.com>
2019-11-08 12:18:26 -0500

    can now create user with ssh key login
    can now create user with ssh key login

--- first_user.sh
+++ first_user.sh
@@ -11,6 +11,83 @@ message_box "Ultimate Crypto-Server Setup Installer" \
 \n\nRunning any application as root is a serious security risk.
 \n\nTherefore we make you create a user account :)"
 
+
+dialog --title "Create New User With SSH Key" \
+--yesno "Do you want to create your new user with SSH key login?
+Selecting no will create user with password login only." 7 60
+response=$?
+case $response in
+   0) UsingSSH=yes;;
+   1) UsingSSH=no;;
+   255) echo "[ESC] key pressed.";;
+esac
+
+if [[ ("$UsingSSH" == "yes") ]]; then
+
+  if [ -z "${yiimpadmin}" ]; then
+  DEFAULT_yiimpadmin=yiimpadmin
+  input_box "New Account Name" \
+  "Please enter your desired user name.
+  \n\nUser Name:" \
+  ${DEFAULT_yiimpadmin} \
+  yiimpadmin
+
+  if [ -z "${yiimpadmin}" ]; then
+  # user hit ESC/cancel
+  exit
+  fi
+  fi
+
+# create random user password
+RootPassword=$(openssl rand -base64 8 | tr -d "=+/")
+
+# prompt for key.
+clear
+echo -e "Please open PuTTY Key Generator on your local machine and generate a new public key."
+read -e -p "Paste your generated key : " ssh_key
+
+clear
+echo -e " Adding new user and setting SSH key...$COL_RESET"
+
+sudo adduser ${yiimpadmin} --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo -e "${RootPassword}\n${RootPassword}" | passwd ${yiimpadmin}
+sudo usermod -aG sudo ${yiimpadmin}
+
+mkdir -p /home/${yiimpadmin}/.ssh
+touch /home/${yiimpadmin}/.ssh/authorized_keys
+chown -R ${yiimpadmin}:${yiimpadmin} /home/${yiimpadmin}/.ssh
+chmod 700 /home/${yiimpadmin}/.ssh
+chmod 644 /home/${yiimpadmin}/.ssh/authorized_keys
+authkeys=/home/${yiimpadmin}/.ssh/authorized_keys
+echo "$ssh_key" > "$authkeys"
+
+# disabling root login and password log in
+
+
+# enabling multipool command
+echo '# yiimp
+  # It needs passwordless sudo functionality.
+  '""''"${yiimpadmin}"''""' ALL=(ALL) NOPASSWD:ALL
+  ' | sudo -E tee /etc/sudoers.d/${yiimpadmin} >/dev/null 2>&1
+
+echo '
+  cd ~/multipool/install
+  bash start.sh
+  ' | sudo -E tee /usr/bin/multipool >/dev/null 2>&1
+sudo chmod +x /usr/bin/multipool
+
+sudo cp -r ~/multipool /home/${yiimpadmin}/
+cd ~
+sudo rm -r multipool
+sudo setfacl -m u:${yiimpadmin}:rwx /home/${yiimpadmin}/multipool
+
+clear
+echo "New User is installed make sure you saved your private key..."
+echo -e "$RED Please reboot system and log in as the new user and type$COL_RESET $GREEN multipool$COL_RESET $RED to continue setup...$COL_RESET"
+exit 0
+
+fi
+
 if [ -z "${yiimpadmin}" ]; then
 DEFAULT_yiimpadmin=yiimpadmin
 input_box "New Account Name" \
@@ -78,6 +155,8 @@ sudo cp -r ~/multipool /home/${yiimpadmin}/
 cd ~
 sudo rm -r multipool
 sudo setfacl -m u:${yiimpadmin}:rwx /home/${yiimpadmin}/multipool
+
+# disabling root login and password log in
 
 clear
 echo "New User is installed..."
