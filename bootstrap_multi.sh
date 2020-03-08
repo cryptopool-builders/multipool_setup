8851ec412043a954c0e20bcab8ac3de6e03d34e1
cryptopool <cryptopool.builders@gmail.com>
2020-03-08 18:03:27 -0400

    Fixed Host Name error
    Fixed Host Name error

--- bootstrap_multi.sh
+++ bootstrap_multi.sh
@@ -8,7 +8,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v1.43
+	TAG=v1.44
 fi
 
 # Clone the MultiPool repository if it doesn't exist.
--- bootstrap_single.sh
+++ bootstrap_single.sh
@@ -8,7 +8,7 @@
 #########################################################
 
 if [ -z "${TAG}" ]; then
-	TAG=v1.84
+	TAG=v1.85
 fi
 
 # Clone the MultiPool repository if it doesn't exist.
